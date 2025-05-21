f19ea719cb44c142baa64f699d05786a68891cd8
Anthony ELUECQUE <anthony76520.ae@gmail.com>
2025-05-21 18:09:52 +0200

    feat: initialized backend configuration

---
+++ backend/src/config/environment/index.ts
@@ -0,0 +1,36 @@
+import { config } from 'dotenv';
+import { Interfaces } from '../../interfaces';
+import path from 'path';
+
+config({ path: path.resolve(__dirname, '../../../../.env') });
+
+const validateEnv = (): Interfaces.Environment => {
+    const requiredVars = [
+        'DB_HOST',
+        'DB_PORT',
+        'DB_NAME',
+        'DB_USER',
+        'DB_PASSWORD',
+        'PORT',
+    ] as const;
+
+    const missingVars = requiredVars.filter(key => !process.env[key]);
+
+    if (missingVars.length > 0) {
+        throw new Error(
+            `The following environment variables are missing: ${missingVars.join(', ')}`,
+        );
+    }
+
+    return {
+        dbHost: process.env.DB_HOST!,
+        dbPort: parseInt(process.env.DB_PORT!, 10)!,
+        dbName: process.env.DB_NAME!,
+        dbUser: process.env.DB_USER!,
+        dbPassword: process.env.DB_PASSWORD!,
+        port: parseInt(process.env.PORT!, 10)!,
+    };
+};
+
+const environment = validateEnv();
+export { environment };
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,41 @@
-console.log("Initialization");
+import "reflect-metadata"
+import { environment } from "./config/environment";
+import { DatabaseConfiguration } from "./config";
+import Fastify from 'fastify';
+import fastifyCors from '@fastify/cors';
+import AutoLoad from '@fastify/autoload';
+import { join } from "path";
+
+const initializeApi = async () => {
+
+    const { port } = environment;
+
+    const orm = 
+        await DatabaseConfiguration.initializeDatabaseFromEnvironmentVariables(
+            environment
+        );
+
+    const fastify = Fastify();
+
+    fastify.decorate('orm', orm);
+
+    await fastify.register(fastifyCors, {
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    });
+
+    await fastify.register(AutoLoad, {
+        dir: join(__dirname, 'routes'),
+    });
+
+    await fastify.listen({
+        port,
+        host: '0.0.0.0',
+    });
+
+    console.log(
+        `\x1b[36m🚀 Server is running at http://localhost:${port}\x1b[0m`,
+    );
+}
+
+initializeApi();
