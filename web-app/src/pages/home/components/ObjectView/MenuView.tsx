42c955529d46a4f3f8c21db3bc7e351e1ec3b05f
Antorakk <95444282+Antorakk@users.noreply.github.com>
2025-06-03 14:28:20 +0200

    feat: fixed tabs scss

--- web-app/src/pages/home/components/ObjectView/MenuView.tsx
+++ web-app/src/pages/home/components/ObjectView/MenuView.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
 import { Tabs, Tab, Typography } from '@mui/material';
+import { useState } from 'react';
 
 interface TabPanelProps {
   children?: React.ReactNode;
@@ -12,41 +12,79 @@ const TabPanel = (props: TabPanelProps) => {
 
   return (
     <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`vertical-tabpanel-${index}`}
-      aria-labelledby={`vertical-tab-${index}`}
-      {...other}
+      	role="tabpanel"
+		hidden={value !== index}
+		id={`vertical-tabpanel-${index}`}
+		aria-labelledby={`vertical-tab-${index}`}
+		{...other}
     >
-      {value === index && (
-        <div className="tab-panel">
-          <Typography>{children}</Typography>
-        </div>
-      )}
+      	{value === index && (
+			<div className="tab-panel">
+				<Typography>{children}</Typography>
+			</div>	
+      	)}
     </div>
   );
 }
 
 export const MenuView = () => {
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = useState(0);
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
+	setValue(newValue);
   };
 
   return (
     <div id="menu--container">
       <div className="tabs-wrapper">
         <Tabs
-          orientation="vertical"
-          value={value}
-          onChange={handleChange}
-          aria-label="Vertical menu tabs"
-          className="vertical-tabs"
+			orientation="vertical"
+			value={value}
+			onChange={handleChange}
+			aria-label="Vertical menu tabs"
+			className="vertical-tabs"
+			sx={{
+				'& .MuiTabs-indicator': {
+				backgroundColor: '#0957FF',
+				}
+			}}
         >
-          <Tab label="Informations générales" />
-          <Tab label="Autres informations" />
-          <Tab label="Autres onglets" />
+          <Tab
+            label="Informations générales"
+            sx={{ 
+				justifyContent: 'flex-start', 
+				alignItems: 'flex-start', 
+				textAlign: 'left',     
+				color: '#000', 
+					'&.Mui-selected': {
+					color: '#0957FF',
+				}
+            }}
+          />
+          <Tab
+            label="Autres informations"
+            sx={{ 
+				justifyContent: 'flex-start', 
+				alignItems: 'flex-start', 
+				textAlign: 'left',     
+				color: '#000', 
+					'&.Mui-selected': {
+					color: '#0957FF',
+				}
+            }}
+          />
+          <Tab
+            label="Autres onglets"
+            sx={{ 
+				justifyContent: 'flex-start', 
+				alignItems: 'flex-start', 
+				textAlign: 'left',     
+				color: '#000', 
+					'&.Mui-selected': {
+					color: '#0957FF',
+				}
+            }}
+          />
         </Tabs>
       </div>
       <button className="export-btn">
