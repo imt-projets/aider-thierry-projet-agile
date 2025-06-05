a8e091a942e7f2621e942b6dd7a03dc833a48504
Anthony ELUECQUE <anthony76520.ae@gmail.com>
2025-06-05 13:44:50 +0200

    feat: fixed tabs

--- web-app/src/pages/home/components/ObjectView/ObjectTabs.tsx
+++ web-app/src/pages/home/components/ObjectView/ObjectTabs.tsx
@@ -27,69 +27,63 @@ const TabPanel = (props: TabPanelProps) => {
   );
 }
 
+interface TabProperties {
+	name : string
+}
+
+const tabs : TabProperties[] = [
+	{ name: "Informations générales" },
+	{ name : "Autres onglets"}
+]
+
+
 export const ObjectTabs = () => {
-  const [value, setValue] = useState(0);
+  	const [value, setValue] = useState(0);
 
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-	setValue(newValue);
-  };
+	const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+		setValue(newValue);
+	};
 
-  return (
-    <div id="menu--container">
-      <div className="tabs-wrapper">
-        <Tabs
-			orientation="vertical"
-			value={value}
-			onChange={handleChange}
-			aria-label="Vertical menu tabs"
-			className="vertical-tabs"
-			sx={{
-				'& .MuiTabs-indicator': {
-				backgroundColor: '#0957FF',
-				}
-			}}
-        >
-          <Tab
-            label="Informations générales"
-            sx={{ 
-				justifyContent: 'flex-start', 
-				alignItems: 'flex-start', 
-				textAlign: 'left',     
-				color: '#000', 
+	const renderTabs = () => {
+		return tabs.map((tab, idx) => (
+			<Tab
+				key={tab.name}
+				label={tab.name}
+				sx={{ 
+					justifyContent: 'flex-start', 
+					alignItems: 'flex-start', 
+					textAlign: 'left',     
+					color: '#000', 
 					'&.Mui-selected': {
 					color: '#0957FF',
-				}
-            }}
-          />
-          <Tab
-            label="Autres informations"
-            sx={{ 
-				justifyContent: 'flex-start', 
-				alignItems: 'flex-start', 
-				textAlign: 'left',     
-				color: '#000', 
-					'&.Mui-selected': {
-					color: '#0957FF',
-				}
-            }}
-          />
-          <Tab
-            label="Autres onglets"
-            sx={{ 
-				justifyContent: 'flex-start', 
-				alignItems: 'flex-start', 
-				textAlign: 'left',     
-				color: '#000', 
-					'&.Mui-selected': {
-					color: '#0957FF',
-				}
-            }}
-          />
-        </Tabs>
-      </div>
-      <button className="export-btn">
-        Export Excel
-      </button>
-    </div>
-  );
+					}
+				}}
+				value={idx}
+			/>
+		))
+	}
+
+	return (
+		<div id="menu--container">
+			<div className="tabs-wrapper">
+				<Tabs
+					orientation="vertical"
+					value={value}
+					onChange={handleChange}
+					aria-label="Vertical menu tabs"
+					className="vertical-tabs"
+					sx={{
+						'& .MuiTabs-indicator': {
+						backgroundColor: '#0957FF',
+						}
+					}}
+				>
+					{renderTabs()}
+				</Tabs>
+			</div>
+			<button className="export-btn">
+				Export Excel
+			</button>
+		</div>
+	);
 };
