b12b4dd13d5afb5fc54d8dbe8c59fa264c1a3a0a
Anthony ELUECQUE <anthony76520.ae@gmail.com>
2025-06-23 17:13:48 +0200

    feat: improved docker env

--- backend-init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-CONTAINER_NAME="backend"
-
-echo "🚀 Exécution de la seed dans le conteneur $CONTAINER_NAME..."
-
-docker exec $CONTAINER_NAME bash -c "cd projet && npm run seed-docker:run"
