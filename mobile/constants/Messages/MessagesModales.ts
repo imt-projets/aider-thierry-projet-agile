917464008888ca9280d2d8c3e172edafd9347715
Abderrazak MESSAOUDI <abderrazak.messaoudi@universite-paris-saclay.fr>
2025-06-26 12:30:06 +0200

    ajustements messages

--- mobile/constants/Messages/MessagesModales.ts
+++ mobile/constants/Messages/MessagesModales.ts
@@ -1,7 +1,7 @@
-export const MESSAGE_HEADER_GO_HOME_TITLE =  'Êtes vous sûr de retourner au menu principal ?';
-export const MESSAGE_HEADER_GO_HOME_BODY = 'L\'action en cours sera abandonée';
+export const MESSAGE_HEADER_GO_HOME_TITLE =  'Êtes-vous sûr de vouloir retourner au menu principal ?';
+export const MESSAGE_HEADER_GO_HOME_BODY = 'L\'action en cours sera abandonnée.';
 
-export const MESSAGE_GO_BACK_HOME_BODY= 'Menu Principal';
+export const MESSAGE_GO_BACK_HOME_BODY= 'Menu principal';
 export const MESSAGE_GO_BACK_SCAN_ROOM_BODY= 'Scan de la salle';
 export const MESSAGE_GO_BACK_SCAN_ITEM_BODY= 'Code barre de la salle récupéré';
 export const MESSAGE_GO_BACK_SCAN_OBJECT_BODY= 'Scan de l\'objet';
